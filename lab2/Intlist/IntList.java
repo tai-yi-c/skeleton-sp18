3fae2eb7e015f09da8c5e0afed0f15065c183219
tai-yi-c <22359625501@qq.com>
2021-08-28 23:13:05 +0800

    final update IntList.java

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,7 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        if(A==null) return null;
+        if(A==null) return B;
         IntList ptr=A;
         while(ptr.rest!=null) {
             ptr=ptr.rest;
@@ -97,7 +97,7 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        if(A==null) return null;
+        if(A==null) return B;
         IntList result=new IntList(A.first,null);
         IntList ptrR=result;
         IntList ptrA=A.rest;
