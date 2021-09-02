dd7195ef5e6c0a8be97191d8e2f77822a846adc8
tai-yi-c <22359625501@qq.com>
2021-09-02 17:29:25 +0800

    establish the NBody.java and Planet.java

---
+++ proj0/NBody.java
@@ -0,0 +1,142 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+//import jdk.javadoc.internal.tool.resources.javadoc;
+
+
+public class NBody {
+    public static int N;//represent existing N planets of the universe
+    public static double R;//represent the radius of the universe
+    public static Planet[] universe;//represent all the planets existing in the universe
+    public static double readRadius(String path) {
+        double result=0.0;
+        int i=0;
+        In in = new In(path);
+        //the radius of the universe is in the 2nd line
+        while(i<2){
+            result=in.readDouble();
+            i++;
+        }
+        return result;
+    }
+    private static boolean isNumber(String num) {
+        try{
+            double inValue = Double.parseDouble(num);
+            return true;
+        }catch(NumberFormatException e) {
+            //do nothing
+        }
+        return false;
+    }
+    public static Planet[] readPlanets(String path) {
+        In in=new In(path);
+        int i=0;
+        //move to the start of the planet data
+        while(i<2) {
+            in.readLine();
+            i++;
+        }
+        
+        Vector<Planet> planets = new Vector<Planet>();
+        //read the data of every planet
+        while(in.hasNextLine()) {
+           
+            //double xP = in.readDouble();
+            String xpS = in.readString();
+            if(!isNumber(xpS)) {
+                break;
+            }
+            double xP = Double.parseDouble(xpS);
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            Double m = in.readDouble();
+            String im = in.readString();
+
+            Planet theP=new Planet(xP, yP, xV, yV, m, im);
+            planets.add(theP);
+
+        }
+        Planet [] planetsArr = new Planet[planets.size()];
+        for(int j=0;j<planets.size();j++) {
+            planetsArr[j] = planets.elementAt(j);
+        }
+        return planetsArr;
+    }
+    /**draw the background of the universe */
+    private static void drawBackground() {
+        String imageToDraw = "images/starfield.jpg";
+        StdDraw.setScale(-R,R);
+
+		/* Clears the drawing window. */
+		StdDraw.clear();
+
+		/* Stamps three copies of advice.png in a triangular pattern. */
+		StdDraw.picture(0, 0, imageToDraw);
+
+		/* Shows the drawing to the screen, and waits 2000 milliseconds. */
+		//StdDraw.show();
+		//StdDraw.pause(2000);
+    }
+    /**draw all the planets */
+    private static void drawPlanets() {
+
+        for(int i = 0;i < N;i++) {
+            String imageToDraw = "images\\" + universe[i].imgFileName;
+            /* Stamps three copies of advice.png in a triangular pattern. */
+            StdDraw.picture(universe[i].xxPos, universe[i].yyPos, imageToDraw);
+            /* Shows the drawing to the screen, and waits 2000 milliseconds. */
+            //StdDraw.show();
+            //StdDraw.pause(200);
+        }
+        
+    }
+    /**create an animation */
+    private static void animation(double T, double dt) {
+        StdDraw.enableDoubleBuffering();
+        
+        double time=0;
+        while(time < T) {
+            
+            drawBackground();
+            drawPlanets();
+            StdDraw.show();
+            StdDraw.pause(5);
+
+            for(Planet p:universe) {
+                double fX = p.calcNetForceExertedByX(universe);
+                double fY = p.calcNetForceExertedByY(universe);
+                p.update(dt, fX, fY);
+                //System.out.printf("star %s: position is (%f,%f) \n",p.imgFileName ,p.xxPos, p.yyPos);
+            }
+            
+            time += dt;
+        }
+    }
+    /**this is the main method of the whole project */
+    public static void main(String[] args){
+        
+        //input T dt filename
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println("T:");
+        //double T = 1e10;
+        //double T = Double.parseDouble(scanner.nextLine());
+        double T = scanner.nextDouble();
+        //System.out.println("dt:");
+        //double dt = 1e5;
+        //double dt = Double.parseDouble(scanner.nextLine());
+        double dt = scanner.nextDouble();
+        //System.out.println("filename:");
+        String filename = scanner.next();
+        //String filename = "data\\awesome.txt";
+        scanner.close();
+
+        R = readRadius(filename) ;
+        universe = readPlanets(filename);
+        N = universe.length;
+        
+        //show the animation
+        animation(T, dt);
+    }
+}
+
---
+++ proj0/Planet.java
@@ -0,0 +1,95 @@
+/**
+ * @author 太易
+ */
+public class Planet {
+    public double xxPos;//Its current x position
+    public double yyPos;//Its current y position
+    public double xxVel;//Its current velocity in the x direction
+    public double yyVel;//Its current velocity in the y direction
+    public double mass;//Its mass
+    //The name of the file that corresponds to the image that depicts the planet
+    public String imgFileName;
+    public static double G=6.67e-11;
+
+    //**constructer of Planet with specific paramester */
+    public Planet(double xP, double yP, double xV,
+    double yV, double m, String im) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = im;
+    }
+    //**constructer of Planet with another Planet P */
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        xxVel = p.xxVel;
+        yyPos = p.yyPos;
+        yyVel = p.yyVel;
+        mass=p.mass;
+        imgFileName=p.imgFileName;
+    }
+    //**calculate the distance between two Planets */
+    public double calcDistance(Planet p) {
+        double idx = this.xxPos - p.xxPos;
+        double idy = this.yyPos - p.yyPos;
+        return Math.sqrt(idx*idx+idy*idy);
+    }
+    //**calculate the force exerted on this planet by the given one */
+    public double calcForceExertedBy(Planet p) {
+        if(p == this) return 0.0;
+        double r2 = this.calcDistance(p) * this.calcDistance(p);
+        return (G * this.mass * p.mass) / r2;
+    }
+    //**calculate the force of x direction exerted on this planet by the given one */
+    public double calcForceExertedByX(Planet p) {
+        if(p == this) return 0.0;
+        double totalForce = this.calcForceExertedBy(p);
+        double idx = p.xxPos - this.xxPos;
+        double dist = this.calcDistance(p);
+        return totalForce * idx / dist;
+    }
+    //**calculate the force of y direction exerted on this planet by the given one */
+    public double calcForceExertedByY(Planet p) {
+        if(p == this) return 0.0;
+        double totalForce = this.calcForceExertedBy(p);
+        double idy = p.yyPos - this.yyPos;
+        double dist = this.calcDistance(p);
+        return totalForce * idy / dist;
+    }
+    //**calculate the total force of x direction exerted on this planet by an array of planet  */
+    public double calcNetForceExertedByX(Planet[] arr) {
+        double totalForceXX=0;
+        for(Planet p : arr) {
+            if(p != this) {
+                totalForceXX += calcForceExertedByX(p);
+            }
+    
+        }
+        return totalForceXX;
+    }
+    //**calculate the total force of y direction exerted on this planet by an array of planet  */
+    public double calcNetForceExertedByY(Planet[] arr) {
+        double totalForceYY=0;
+        for(Planet p : arr) {
+            if(p != this) {
+                totalForceYY += calcForceExertedByY(p);
+            }
+            
+        }
+        return totalForceYY;
+    }
+    //**update to get the new position of the planet because of the gravity from other planets */
+    public void update(double dt,double fX,double fY) {
+        //calculate the acceleration of x,y dirction
+        double aX = fX / this.mass;
+        double aY = fY / this.mass;
+        //calculate the velocity of x,y direction
+        this.xxVel += dt * aX;
+        this.yyVel += dt * aY;
+        //calculate the new position 
+        this.xxPos += dt * this.xxVel;
+        this.yyPos += dt * this.yyVel;
+    }
+}
